af88e81c315e1427ed05b15743364bec2a7a9155
jay.gao <jay.gao@alo7.com>
2018-07-24 09:54:20 +0800

    feat: enable authentication from scratch
    1. use bcrypt gem

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  attr_accessor :password
+
+  validates_confirmation_of :password, :on => :create
+  validates_uniqueness_of :email
+  validates_presence_of :password
+  validates_presence_of :email
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if(password.present?)
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    end
+  end
+end
