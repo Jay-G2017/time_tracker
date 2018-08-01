21f7d962eff40bbf6e87f794115213f7221ca2bf
jay.gao <jay.gao@alo7.com>
2018-08-01 08:54:45 +0800

    feat: 对projects和category增加user_id关联
    1. 改了seed.
    2. 只显示与current_user相关的projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   validates_presence_of :email
 
+  has_many :categories
+  has_many :projects
+
+  def pinned_projects 
+    projects.where(pin: true)
+  end
+
 end
