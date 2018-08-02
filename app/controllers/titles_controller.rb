9af16aada8c6aea87325d8d8e1aa0b1fca2ff3f0
jay.gao <jay.gao@alo7.com>
2018-08-02 08:35:55 +0800

    feat: 可以create title

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -4,4 +4,15 @@ class TitlesController < ApplicationController
     @title = Title.new
     render partial: 'new'
   end
+
+  def create
+    @project = Project.find params[:project_id]
+    @title = @project.titles.create!(title_params)
+    render partial: 'show'
+  end
+
+  private
+  def title_params
+    params.require(:title).permit(:name)
+  end
 end
