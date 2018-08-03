57f3320aacadaabc8e645d81212a9a8a8d23c252
jay.gao <jay.gao@alo7.com>
2018-08-03 08:53:49 +0800

    feat: 可以编辑title
    1. 把title show partial共享出来.

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -5,10 +5,23 @@ class TitlesController < ApplicationController
     render partial: 'new'
   end
 
-  def create
+  def edit
     @project = Project.find params[:project_id]
-    @title = @project.titles.create!(title_params)
-    render partial: 'show'
+    @title = Title.find params[:id]
+    render partial: 'edit'
+  end
+
+  def update
+    project = Project.find params[:project_id]
+    title = Title.find params[:id]
+    title.update!(title_params)
+    render partial: 'shared/title_show', locals: { project: project, title: title }
+  end
+
+  def create
+    project = Project.find params[:project_id]
+    title = project.titles.create!(title_params)
+    render partial: 'shared/title_show', locals: { project: project, title: title }
   end
 
   def destroy
